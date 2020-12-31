33afcd20306b1dde4fdede29a5166538e345e846
Jonas Nilsson <mjc.nilsson@gmail.com>
2020-12-31 13:48:37 +0100

    Working on showing images.

--- camera/views.py
+++ camera/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render, loader, HttpResponse
 from django.views.decorators.cache import never_cache
 from camera.models import Camera, CameraImage
 from temperature_monitor_site.settings import IMAGE_STORAGE_ROOT
+from datetime import datetime
+import pytz
+import tzlocal
 
 
 @never_cache
 def show_images(request):
     index_page_template = loader.get_template("show_images.html")
     all_cameras = Camera.objects.all()
-    ret_dict = {"cameras": all_cameras}
+    now = datetime.now().utcnow().replace(tzinfo=pytz.utc)
+    local_TZ = tzlocal.get_localzone()
+    ret_dict = {"cameras": all_cameras, "now":now.astimezone(local_TZ).strftime("%Y-%m-%d, %H:%M:%S")}
     return HttpResponse(index_page_template.render(ret_dict), content_type="text/html")
 
 
