14011090b2278d6711fc3b59c71dcf112a43820f
Jonas Nilsson <mjc.nilsson@gmail.com>
2021-01-02 01:07:03 +0100

    Somewhat working image display.

--- camera/urls.py
+++ camera/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
     path('show_images/', views.show_images),
     path('get_image/<int:image_id>/', views.get_image),
+    path('get_timestamp/<int:image_id>/', views.get_timestamp),
+    path('get_image_id/<int:camera_id>/<int:timestamp>/', views.get_image_id),
+    re_path(r'^get_offset_image_id/(?P<image_id>[0-9]+)/(?P<offset>-?[0-9]+)/$', views.get_offset_image_id),
 ]
--- camera/views.py
+++ camera/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, loader, HttpResponse
 from django.views.decorators.cache import never_cache
 from camera.models import Camera, CameraImage
 from temperature_monitor_site.settings import IMAGE_STORAGE_ROOT
-from datetime import datetime
+from datetime import datetime, timedelta
+from django.core.exceptions import FieldError
 import pytz
 import tzlocal
+import json
 
 
 @never_cache
@@ -23,3 +25,62 @@ def get_image(request, image_id):
     image_data = in_file.read()
     in_file.close()
     return HttpResponse(content=image_data, content_type="image/jpeg")
+
+
+def get_image_id(request, camera_id: int, timestamp: int):
+    used_timestamp = datetime.fromtimestamp(timestamp, pytz.utc)
+    return HttpResponse(content=str(get_image_id_with_timestamp(camera_id, used_timestamp)), content_type="text/text")
+
+
+def get_image_id_with_timestamp(camera_id: int, timestamp: datetime):
+    camera_images = CameraImage.objects.filter(camera_id=camera_id)
+    greater = camera_images.filter(timestamp__gte=timestamp).order_by("timestamp").first()
+    less = camera_images.filter(timestamp__lte=timestamp).order_by("-timestamp").first()
+    if greater and less:
+        used_image = greater if greater.timestamp - timestamp < timestamp - less.timestamp else less
+    else:
+        used_image = greater or less
+    return used_image.id
+
+
+def get_next_image_id(image_id):
+    current_image_entry = CameraImage.objects.filter(id=image_id).get()
+    try:
+        next_image = CameraImage.objects.filter(camera_id=current_image_entry.camera_id, id_gt=current_image_entry.id).order_by("timestamp").first()
+    except FieldError:
+        return current_image_entry.id
+    if not next_image:
+        return current_image_entry.id
+    return next_image.id
+
+
+def get_previous_image_id(image_id):
+    current_image_entry = CameraImage.objects.filter(id=image_id).get()
+    try:
+        previous_image = CameraImage.objects.filter(camera_id=current_image_entry.camera_id, id_lt=current_image_entry.id).order_by("-timestamp").first()
+    except FieldError:
+        return current_image_entry.id
+    if not previous_image:
+        return current_image_entry.id
+    return previous_image.id
+
+
+def get_next_image_id_with_offset(image_id: int, time_offset: timedelta):
+    current_image_entry = CameraImage.objects.filter(id=image_id).get()
+    next_id = get_image_id_with_timestamp(camera_id=current_image_entry.camera_id, timestamp=current_image_entry.timestamp + time_offset)
+    if next_id != current_image_entry.id:
+        return next_id
+    if time_offset >= timedelta(seconds=0):
+        return get_next_image_id(image_id)
+    return get_previous_image_id(image_id)
+
+
+def get_offset_image_id(request, image_id: str, offset: str):
+    return HttpResponse(content=str(get_next_image_id_with_offset(int(image_id), timedelta(seconds=int(offset)))), content_type="text/json")
+
+
+def get_timestamp(request, image_id):
+    current_image_entry = CameraImage.objects.filter(id=image_id).get()
+    local_TZ = tzlocal.get_localzone()
+    json_data = {"timestamp": int(current_image_entry.timestamp.timestamp()), "datetime_string":current_image_entry.timestamp.astimezone(local_TZ).strftime("%Y-%m-%d, %H:%M:%S")}
+    return HttpResponse(content=json.dumps(json_data), content_type="text/json")
